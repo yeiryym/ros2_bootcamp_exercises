29db7abd407d10dcf9133994e8db70dd1b2c6f6f
Yeiry Melendez <yymelendez@ucsd.edu>
2024-10-16 17:39:56 +0000

    Add new ROS 2 package turtle_sim

---
+++ turtle_sim/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtle_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='yymelendez@ucsd.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = turtle_sim.my_node:main'
+            'turtle_cleaner = turtle_sim.turtle_cleaner:main'
+        ],
+    },
+)
---
+++ turtle_sim/turtle_sim/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from turtle_sim.')
+
+
+if __name__ == '__main__':
+    main()
